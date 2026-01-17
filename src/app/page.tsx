df33a2cb4376f2893991ae140a6266902d900882
RyanLeungTsu <ryanleungtsu@gmail.com>
2026-01-17 12:31:15 -0800

    added SlideCanvas.tsx and pivoted away from text based slides to a canvas slide to allow multimedia additions for editing slides. key changes include moving to element based instead of text based.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import JeopardyGrid from "../components/JeopardyGrid";
 import EditToggle from "../components/EditToggle";
-
+// NOTE: try and cahgne structure to support multiple media files and make the edit screen for question more like a canvas instead of current text based limited cells. 
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-gray-100">
